bfabcd1c4a1c04fceba6daa4172c5edb732b8004
hackzy <hufazyj@gmail.com>
2023-09-22 17:59:20 +0800

    backup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name='askCheckSum',
+      version = '1.0.0',
+      description = 'askCheckSum',
+      packages=find_packages(),
+      author_email='hufazyj@gmail.com',
+      author='hackzy',
+)
